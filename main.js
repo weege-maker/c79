1a953834d486d2e86163277690770cce1b6f15b8
weege-maker <132228403+weege-maker@users.noreply.github.com>
2023-07-08 19:00:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+nombre_alumno_array = [];
+   
+    function submit()
+    {
+       
+        var nombre_1 = document.getElementById("nombre_alumno_1").value;
+        var nombre_2 = document.getElementById("nombre_alumno_2").value;
+        var nombre_3 = document.getElementById("nombre_alumno_3").value;
+        var nombre_4 = document.getElementById("nombre_alumno_4").value;
+
+
+        nombre_alumno_array.push(nombre_1);
+        nombre_alumno_array.push(nombre_2);
+        nombre_alumno_array.push(nombre_3);
+        nombre_alumno_array.push(nombre_4);
+
+
+        console.log(nombre_alumno_array);
+       
+        document.getElementById("ver_nombre").innerHTML = nombre_alumno_array;
+        document.getElementById("boton_enviar").style.display = "none";
+        document.getElementById("boton_ordenar").style.display = "inline-block";
+
+
+    }
+
+
+function sorting()
+{
+    nombre_alumno_array.sort();
+    console.log(nombre_alumno_array);
+    document.getElementById("ver_nombre").innerHTML = nombre_alumno_array;
+}
+
+
